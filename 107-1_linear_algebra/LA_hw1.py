22e3f2a1a777e4a68df9639d8383dedc65bdc319
wei <t104306033@ntut.org.tw>
2018-09-19 01:16:37 +0800

    /107-1 linear_algebra: add the class Matrix and shape of form

--- 107-1_linear_algebra/LA_hw1.py
+++ 107-1_linear_algebra/LA_hw1.py
@@ -37,40 +37,63 @@ Created on Mon Sep 17 21:16:32 2018
 """
 
 
-
+from collections import Iterable
 
 S = [[2, -1, 3], 
      [1, 1, 3]]
+
 
 class Array(object):
 #    pointer = 0
     
     def __init__(self, obj):
-        print(obj)
+#        print(obj)
         self.obj = obj
+    
+    @property
+    def _num(self):
+        if isinstance(self.obj, Iterable):
+            if isinstance(self.obj[0], Iterable):
+                return (len(self.obj), len(self.obj[0]))
+            else:
+                return (len(self.obj), )
+    
 
+    def scaling():
+        pass
+    
+
+    
+    def __repr__(self):
+        _ = "\n      ".join([str(_) for _ in self.obj])
+        return "array({})".format(_)
+    
+    def __str__(self):
+        return "\n ".join([str(_) for _ in self.obj])
+
+
+
+class Matrix(Array):
+    def __init__(self, array):
+        super(Matrix, self).__init__(array)
+#        self.array = array
+#        self.pivot_col_pointer = 0
+    
+    @property
+    def form(self):
+        return (self._num)
+    
     def check_leader():
+    # sort the order of array, if leadering entry
+        pass
+
+    def replacement():
         pass
     
     def interchange():
         pass
     
-    def scaling():
-        pass
-    
-    def replacement():
-        pass
-    
-    def __repr__(self):
-        _ = "\n      ".join([str(_) for _ in S])
-        return "array({})".format(_)
-    
-    def __str__(self):
-        return "\n".join([str(_) for _ in S])
-        
 
-#class Matrix(Array):
-#    def __init__(self):
-#        pass
+a = Matrix(S)
 
-a = Array(S)
+print(a.form)
